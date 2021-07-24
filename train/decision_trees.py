94325f675990bad8839b64d6a67f72c031deacf4
Azariagmt <azariagebremichael10@gmail.com>
2021-07-24 20:19:35 +0300

    removes version for rev error

--- train/decision_trees.py
+++ train/decision_trees.py
@@ -26,35 +26,40 @@ logger = logging.getLogger(__name__)
 
 path = 'data/AdSmartABdata.csv'
 repo = "../"
-version = "fae650c01c6e0f6eea08898e15da1dc703572729"
+version = "'v3'"
+# return to normal tag version and print in markdown
 
 data_url = dvc.api.get_url(
     path=path,
     repo=repo,
 )
 
-mlflow.set_experiment('Decision Trees experiment')
+mlflow.set_experiment('Ad Campaign Analysis')
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     np.random.seed(50)
-    platform_os_df = pd.read_csv('../data/AdSmartABdata.csv', index_col=0)
+    df = pd.read_csv('../data/AdSmartABdata.csv', index_col=0)
     mlflow.log_param('data_url', data_url)
     mlflow.log_param('data_version', version)
-    mlflow.log_param('input_rows', platform_os_df.shape[0])
-    mlflow.log_param('input_cols', platform_os_df.shape[1])
+    mlflow.log_param('input_rows', df.shape[0])
+    mlflow.log_param('input_cols', df.shape[1])
+    mlflow.log_param('model_type','Decision Trees')
 
-    len(platform_os_df['device_make'].unique())
+    len(df['device_make'].unique())
 
     def group_brand(brand):
-        device_makes = platform_os_df['device_make'].unique()
+        '''
+        Groups several individual phones into one manufacturer group
+        '''
+        device_makes = df['device_make'].unique()
         for device_make in device_makes:
             if brand in device_make.lower():
                 # print(device_make)
-                # print(platform_os_df['device_make'][platform_os_df['device_make']== device_make] )
-                platform_os_df['device_make'][platform_os_df['device_make']
-                                              == device_make] = brand
-        # print(len(platform_os_df['device_make'].unique()))
+                # print(df['device_make'][df['device_make']== device_make] )
+                df['device_make'][df['device_make']
+                                  == device_make] = brand
+        # print(len(df['device_make'].unique()))
 
     known_brands = ['lg', 'nokia', 'iphone', 'pixel', 'xiaomi', 'oneplus', 'htc', 'samsung', 'vog', 'vfd', 'pot', 'moto', 'mrd', 'MAR-LX1A',
                     'huawei', 'ANE-LX1', 'CLT-L09', 'ELE-L09', 'I3312']
@@ -62,48 +67,44 @@ if __name__ == '__main__':
     for known_brand in known_brands:
         group_brand(known_brand)
 
-    platform_os_df['device_make'].unique()
-    phone_group = platform_os_df.groupby('device_make')
+    df['device_make'].unique()
+    phone_group = df.groupby('device_make')
     phone_group['device_make'].value_counts()
 
-    for platform in platform_os_df['device_make'].unique():
+    for platform in df['device_make'].unique():
         print(platform)
         group_brand(platform)
 
-    platform_os_df['device_make'].unique()
-    len(platform_os_df['device_make'].unique())
-    for grouped_platform in platform_os_df['device_make'].unique():
+    df['device_make'].unique()
+    len(df['device_make'].unique())
+    for grouped_platform in df['device_make'].unique():
         if grouped_platform not in known_brands:
             print(grouped_platform)
-            platform_os_df['device_make'][platform_os_df['device_make']
-                                          == grouped_platform] = "Generic Smartphone"
+            df['device_make'][df['device_make']
+                              == grouped_platform] = "Generic Smartphone"
         else:
             pass
 
-    print(len(platform_os_df['device_make'].unique()))
+    print(len(df['device_make'].unique()))
 
-    platform_os_df.head()
+    df.head()
 
     ordinal_encoder = OrdinalEncoder()
     ordinal_encode_columns = ['experiment', 'date', 'device_make']
-    platform_os_df[ordinal_encode_columns] = ordinal_encoder.fit_transform(
-        platform_os_df[ordinal_encode_columns])
-    platform_os_df.head()
+    df[ordinal_encode_columns] = ordinal_encoder.fit_transform(
+        df[ordinal_encode_columns])
+    df.head()
 
-    platform_os_df[["experiment", "date",
-                    "hour",	"device_make"	, "platform_os"]]
+    X = df.drop("answer", axis=1)
 
     scaler = StandardScaler()
-    scaler.fit(platform_os_df[["experiment", "date",
-               "hour",	"device_make"	, "platform_os"]])
-    zz = scaler.transform(
-        platform_os_df[["experiment", "date",	"hour",	"device_make"	, "platform_os"]])
-    scaled_features_df = pd.DataFrame(zz, index=platform_os_df.index, columns=platform_os_df[[
-                                      "experiment", "date",	"hour",	"device_make"	, "platform_os"]].columns)
+    scaler.fit(X)
+    zz = scaler.transform(X)
+    scaled_features_df = pd.DataFrame(zz, index=df.index, columns=X.columns)
     scaled_features_df.head()
 
     scaled_features_df = pd.concat(
-        [scaled_features_df, platform_os_df.answer], axis=1)
+        [scaled_features_df, df.answer], axis=1)
 
     X_train, y_train, X_valid, y_valid, X_test, y_test = train_valid_test_split(scaled_features_df, target='answer',
                                                                                 method='sorted', sort_by_col='date',
@@ -118,10 +119,10 @@ if __name__ == '__main__':
     print(acc)
     mlflow.log_param('acc', acc)
     clf.get_depth()
-    with open("metrics.txt", 'w') as outfile:
+    with open("dt_metrics.txt", 'w') as outfile:
         outfile.write("Accuracy: " + str(acc) + "\n")
 
     # Plot it
     disp = plot_confusion_matrix(
         clf, X_test, y_test, normalize='true', cmap=plt.cm.Blues)
-    plt.savefig('confusion_matrix.png')
+    plt.savefig('dt_confusion_matrix.png')
